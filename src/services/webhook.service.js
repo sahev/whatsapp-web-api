5ddc6242ac04cc965febf2cb3a567e334f49627a
sahev <s4mus.sh@gmail.com>
2025-08-15 21:47:12 -0300

    add log

--- src/services/webhook.service.js
+++ src/services/webhook.service.js
@@ -6,6 +6,7 @@ export async function trigger(config, event, data) {
   for (const webhook of config.webhooks) {
     if (webhook.events.includes(event)) {
         axios.post(webhook.url, { event, data, instance: config.key }).catch(() => {});
+        console.log(`[${config.key}][${webhook.url}] webhook called`);
     }
   }
 }
